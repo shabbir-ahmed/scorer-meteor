5430b5adae3591cef995f886d4ed113d12f6f786
shabbir3s <shabbir@salahsoftwaresolution.com>
2018-11-04 15:11:12 +0600

    Add dynamic JSX & dynamic players list

--- client/main.js
+++ client/main.js
@@ -4,8 +4,36 @@ import { Meteor } from 'meteor/meteor';
 
 import './main.html'
 
+const players = [{
+  _id: '1',
+  name: 'Shabbir',
+  score: 77
+},{
+  _id: '2',
+  name: 'Korey',
+  score: 55
+},{
+  _id: '3',
+  name: 'Lauren',
+  score: -12
+}];
+
+const renderPlayers = function (playersList) {
+  return playersList.map(function (player) {
+    return <p key={player._id}>{player.name} has {player.score} point(s).</p>;
+  });
+};
+
 Meteor.startup(function (){
+  let title = "Welcome to Score Keep";
   let name = "Shabbir Ahmed";
-  let jsx = <p>Hello {name}!</p>;
+  let jsx = (
+    <div>
+      <h1>{title}</h1>
+      <p>Hello {name}!</p>
+      <p>This is my second p.</p>
+      {renderPlayers(players)}
+    </div>
+  );
   ReactDOM.render(jsx, document.getElementById('app'));
 });
